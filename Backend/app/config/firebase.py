74711c2584f746b394a864a5d5cf71cf328420e4
Gabriel Borela <gabrielborela@MacBook-Air-de-Gabriel.local>
2026-06-01 11:49:29 -0300

    adiciona firebase admin module

--- Backend/app/config/firebase.py
+++ Backend/app/config/firebase.py
@@ -1,13 +1,33 @@
+import os
+import json
+
 import firebase_admin
 
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-
-cred = credentials.Certificate(
+if os.path.exists(
     "app/firebase/serviceAccountKey.json"
-)
+):
 
-firebase_admin.initialize_app(cred)
+    cred = credentials.Certificate(
+        "app/firebase/serviceAccountKey.json"
+    )
+
+else:
+
+    firebase_credentials = json.loads(
+        os.getenv(
+            "FIREBASE_CREDENTIALS"
+        )
+    )
+
+    cred = credentials.Certificate(
+        firebase_credentials
+    )
+
+firebase_admin.initialize_app(
+    cred
+)
 
 db = firestore.client()
